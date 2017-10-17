a5b1c38b5735c236d92521d999fedab1feaf1aa9
Radoslaw Burkacki <radekb333@gmail.com>
2017-10-17 23:39:56 +0100

    database fix

--- src/main/java/radoslawburkacki/honoursproject/familycentre/FamilycentreApplication.java
+++ src/main/java/radoslawburkacki/honoursproject/familycentre/FamilycentreApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FamilycentreApplication {
+public class FamilycentreApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FamilycentreApplication.class, args);
